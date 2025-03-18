581e31049b79eca05959f116d34d0e6957ab2cce
Van1099 <1@ya.ru>
2025-03-18 20:12:05 +0400

    Try

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        speed: 1200,
+		/* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/buttons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/buttons/right.svg"></button>'
+      });
+
+
+});
+
+
+  
