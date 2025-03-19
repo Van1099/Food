a12a2b38da66e71b6dfc9a453269cea8628e2265
Van1099 <1@ya.ru>
2025-03-19 18:56:45 +0400

    Favicon, scroll

--- src/js/script.js
+++ src/js/script.js
@@ -8,9 +8,18 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/buttons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/buttons/right.svg"></button>'
       });
-
-
 });
 
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 500 ){
+        $('.pageup').fadeIn(); 
+        } else {
+        $('.pageup').fadeOut();
+        }
+    });
 
-  
+$("a[href^='#']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+});
